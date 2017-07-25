55400155bc3e2c2bbc3c3c5448b887bc939c4cc0
Jonathan Iqueda <jonathaniqueda@me.com>
2017-07-25 13:59:24 -0300

    novas features

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,6 +15,19 @@
                 <!-- Menu Area Start -->
                 <div class="col-sm-10 col-xs-12">
                     <div class="main_menu_area">
+                        <div class="mainmenu">
+                            <nav>
+                                <ul id="nav">
+                                    <li class="current_page_item"><a href="#inicio">Início</a></li>
+                                    <li><a href="#sobre-nos">Sobre Nós</a></li>
+                                    <li><a href="#servicos">Serviços</a></li>
+                                    <li><a href="#precos">Preços</a></li>
+                                    <li><a href="#clientes">Clientes</a></li>
+                                    <li><a href="#contato">Contato</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+
                         <!-- Side Navigation Area Start -->
                         <div class="menu_open_close">
                             <span></span>
@@ -72,26 +85,25 @@
             <div class="welcome_text welcome_fade_text text-center">
                 <div class="cd-intro default-title">
                     <h2 class="cd-headline clip is-full-width">
-                        <span>FichaRápidaBR Sua Solução em</span><br/>
+                        <span>Ficha Rápida BR sua solução quando o assunto é </span><br/>
                         <span class="cd-words-wrapper">
                                 <b class="is-visible">Pesquisas Restritivas</b>
-                                <b>Assesoria a Corretores de Imóveis</b>
                                 <b>Serviços de Certidões</b>
-                                <b>Assesoria a Administradores de Imóveis</b>
 			                </span>
                     </h2>
                 </div>
                 <div class="s-obj default-description">
                     <p>
-                        Somos Consultores Especializados em <strong>Serviços de Certidões em Todo Brasil, Assesorar
-                            Administradores de Imóveis e Corretores Imobiliários.</strong>
+                        Somos Consultores Especializados em <strong>Serviços de Certidões em Todo Brasil, Assessoramento
+                            de Administradores de Imóveis e Corretores Imobiliários.</strong>
                         <br><br>
                         Conheça os Nossos Serviços.
                         <br><br>
                     </p>
                 </div>
-                <a data-scrollto="sobre-nos" class="s-obj btn royal-btn" id="btn-one-1">Sobre Nós</a>
-                <a data-scrollto="contato" class="s-obj btn royal-btn" id="btn-one-2">Entre em Contato</a>
+                <div class="col-xs-12">
+                    <a data-scrollto="sobre-nos" class="s-obj btn royal-btn" id="btn-one-1">Conheça a Ficha Rápida BR</a>
+                </div>
             </div>
         </div>
     </div>
